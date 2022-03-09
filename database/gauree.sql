9d51283de389bc05ac95628084b1da1da8e4b832
subhanjana99 <48948716+subhanjana99@users.noreply.github.com>
2022-03-10 02:21:12 +0600

    database

--- database/gauree.sql
+++ database/gauree.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 09, 2022 at 01:08 PM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
+-- Host: 127.0.0.1
+-- Generation Time: Mar 09, 2022 at 09:13 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,14 +27,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin_info`
 --
 
-DROP TABLE IF EXISTS `admin_info`;
-CREATE TABLE IF NOT EXISTS `admin_info` (
-  `admin_id` int(30) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin_info` (
+  `admin_id` int(30) NOT NULL,
   `adminName` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  PRIMARY KEY (`admin_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `Password` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin_info`
@@ -46,18 +44,39 @@ INSERT INTO `admin_info` (`admin_id`, `adminName`, `email`, `Password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customer_info`
+--
+
+CREATE TABLE `customer_info` (
+  `CustomerID` int(100) NOT NULL,
+  `CustomerName` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `PhoneNumber` varchar(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer_info`
+--
+
+INSERT INTO `customer_info` (`CustomerID`, `CustomerName`, `Email`, `Password`, `Address`, `PhoneNumber`) VALUES
+(1, 'Subhanjana Dhar', 'subhanjana@gnail.com', '333', 'Jahangirnagar University,Savar,Dhaka', '+8801731916325'),
+(2, 'ridhi ', 'ridhi@yahoo.com', '1234', 'Savar,Dhaka', '+8801731916879');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product_info`
 --
 
-DROP TABLE IF EXISTS `product_info`;
-CREATE TABLE IF NOT EXISTS `product_info` (
-  `proID` int(30) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product_info` (
+  `proID` int(30) NOT NULL,
   `proName` varchar(255) NOT NULL,
   `price` int(255) NOT NULL,
   `piece` int(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  PRIMARY KEY (`proID`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `image` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product_info`
@@ -68,6 +87,50 @@ INSERT INTO `product_info` (`proID`, `proName`, `price`, `piece`, `image`) VALUE
 (2, 'German Silver Set', 500, 10, '1646580780_german silver set.jpg'),
 (3, 'Chokar', 230, 5, '1646590317_chokar.jpg'),
 (4, 'Payel', 120, 6, '1646593121_payel.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_info`
+--
+ALTER TABLE `admin_info`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `customer_info`
+--
+ALTER TABLE `customer_info`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `product_info`
+--
+ALTER TABLE `product_info`
+  ADD PRIMARY KEY (`proID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_info`
+--
+ALTER TABLE `admin_info`
+  MODIFY `admin_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customer_info`
+--
+ALTER TABLE `customer_info`
+  MODIFY `CustomerID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `product_info`
+--
+ALTER TABLE `product_info`
+  MODIFY `proID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
