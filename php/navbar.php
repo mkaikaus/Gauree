2c76c2827cecff8da0562fbe1d7f18b7480e93ed
RishaUrbana <97944004+RishaUrbana@users.noreply.github.com>
2022-03-11 11:47:42 +0600

    navbar

---
+++ php/navbar.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gauree</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+
+
+
+
+
+  <!--Start of a navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-color sticky-top">
+    <a class="navbar-brand" href="#">
+      <img src="../media/banner1 (2).png" class="img-fluid logo-image" alt="Responsive image"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto nav-margin">
+        <li class="nav-item active">
+          <a class="nav-link nav-color ac-color animate__animated animate__zoomIn wow zoomIn" href="#">Home <span
+              class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-color animate__animated animate__zoomIn wow zoomIn " href="#">Product</a>
+        </li>
+
+       <li class="nav-item">
+          <a class="nav-link nav-color animate__animated animate__zoomIn wow zoomIn " href="#">Blog</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="#"> About Us <span
+              class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="#">Contact Us <span
+              class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="#"> Delivery Rules <span
+              class="sr-only">(current)</span></a>
+        </li>
+
+        <li class="nav-item active">
+          <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="#">FAQS <span
+              class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="#">Admin Login
+            <span class="sr-only">(current)</span></a>
+        </li>
+             <li class="nav-item active">
+          <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="#">Customer- Login/Signup <span
+              class="sr-only">(current)</span></a>
+        </li>
+
+
+
+      </ul>
+
+    </div>
+  </nav>
+  <!--end of a navbar-->
+
+
+
+      <script src="../js/jquery-3.5.1.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="../js/wow.js"></script>
+    <script>
+    new WOW.init();
+    </script>
+    <script src="../js/main.js"></script>
+</body>
+</html>
