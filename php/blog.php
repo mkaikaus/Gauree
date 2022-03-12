5d115dc377ceff7170ec15056db6c6d1fc29bc6c
Monisasaha <monisasaha077@gmail.com>
2022-03-12 19:25:18 +0600

    update navber

--- php/blog.php
+++ php/blog.php
@@ -46,8 +46,60 @@ if ($connect) {
 </script>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-color sticky-top">
+        <a class="navbar-brand" href="#">
+            <img src="../media/banner1 (2).png" class="img-fluid logo-image" alt="Responsive image"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div data-include="navbar"></div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto nav-margin">
+                <li class="nav-item active">
+                    <a class="nav-link nav-color ac-color animate__animated animate__zoomIn wow zoomIn" href="../php/Home.php">Home <span
+                class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-color animate__animated animate__zoomIn wow zoomIn " href="../php/product.php">Product</a>
+                </li>
+
+
+                <li class="nav-item active">
+                    <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="../html/about.html"> About Us <span
+                class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle nav-color " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Details
+                </a>
+                    <div class="dropdown-menu dp" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="../html/blog.html">Blog </a>
+                        <a class="dropdown-item" href="../html/rules.html">Delivery Rules</a>
+                        <a class="dropdown-item" href="../html/FAQ.html">FAQS</a>
+                    </div>
+                </li>
+
+                <li class="nav-item active">
+                    <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="../html/contact.html">Contact Us <span
+                class="sr-only">(current)</span></a>
+                </li>
+
+
+                <li class="nav-item active">
+                    <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="../php/admin_login.php">Admin Login
+              <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link nav-color  animate__animated animate__zoomIn wow zoomIn" href="../php/customer_login.php">Customer- Login/Signup <span
+                class="sr-only">(current)</span></a>
+                </li>
+
+
+
+            </ul>
+
+        </div>
+    </nav>
 
     <div class="hero-image">
         <div class="hero-text">
